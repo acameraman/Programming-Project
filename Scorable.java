344045d3b3034bca047c32c50ddb3f4b3f0b66a8
CookieKookie <203384813TK@gmail.com>
2025-04-28 13:41:21 -0400

    Update Scorable.java

--- Scorable.java
+++ Scorable.java
@@ -1,4 +1,12 @@
+/**
+  * This interface includes a method to get the user's score
+ */
 interface Scorable
 {
+      /**
+      * Gets score
+      * @param answer the user's answer
+      * @return the score as an integer
+     */
     int getScore(String answer);
 }
